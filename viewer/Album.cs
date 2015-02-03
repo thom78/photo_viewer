033ef40fecc4c1faefab566a78c8fe4002cda9b6
PHLF <ph.lefur@gmail.com>
2015-02-03 12:25:05 +0100

    Création classe Album, pour une future sérialisation d'une liste d'Albums.

---
+++ viewer/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viewer
+{
+    class Album
+    {
+        public String nom;
+        public List<String> chemin_photos;
+    }
+}
