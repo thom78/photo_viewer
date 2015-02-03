90b1b103b5e055617081cc74c36d79b8303302df
valentinbe <berthomi@ece.fr>
2015-02-03 22:13:43 +0100

    Ajout class Album, classe Picture Creation fenetre ListeAlbums -> fonction daffichage d'images en grid Next step: creations dalbums, linkage du code avec celui d'amin pour import de fichier puis ajout a l'album "pellicule".

--- viewer/Album.cs
+++ viewer/Album.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace viewer
 {
-    class Album
+    class Album : IComparable<Album>
     {
-        public String nom;
-        public List<String> chemin_photos;
+        #region Properties
+
+        [XmlElement(ElementName = "Titre")]
+        public String Title { get; set; }
+
+        [XmlElement(ElementName = "Sous-Titre")]
+        public String SubTitle { get; set; }
+
+        [XmlAttribute(AttributeName = "Date")]
+        public String Date { get; set; }
+
+        //liste des photos contenues dans l'album
+        public List<Picture> Pictures;
+
+        #endregion Properties
+
+        #region Constructor(s)
+
+        public Album()
+        {
+        }
+
+        public Album(String title, String subtitle, String date)
+        {
+            this.Title = title;
+            this.SubTitle = subtitle;
+            this.Date = date;
+        }
+
+        public Album(String title)
+        {
+            this.Title = title;
+        }
+
+        #endregion Constructor(s)
+
+        #region Method_Sort
+
+        int IComparable<Album>.CompareTo(Album other)
+        {
+            return this.Title.CompareTo(other.Title);
+        }
+
+        #endregion Method_Sort
+
+        //creation d'une liste chainee d'Albums
+        public static List<Album> Albums = new List<Album>();
     }
-    ///pooopop
 }
--- viewer/Program.cs
+++ viewer/Program.cs
@@ -16,7 +16,7 @@ namespace viewer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ListaAlbums());
         }
     }
 }
