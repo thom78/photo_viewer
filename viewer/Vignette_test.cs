1dcb60e2179cadb8284973532859be01583ce4d1
PHLF <ph.lefur@gmail.com>
2015-02-17 21:11:58 +0100

    Création branche développement pour l'interface

---
+++ viewer/Vignette_test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace viewer
+{
+    public partial class Vignette_test : UserControl
+    {
+        public Vignette_test()
+        {
+            InitializeComponent();
+        }
+    }
+}
