3090c3d17e04765edf350d53fcaae0a5ebd43b8b
thom78 <thomthom78@live.fr>
2015-02-21 12:22:28 +0100

    Remise en forme plus potable pour P-H

--- viewer/GUI/Forms/Diapo.cs
+++ viewer/GUI/Forms/Diapo.cs
@@ -13,96 +13,87 @@ namespace viewer
     public partial class Diapo : Form
     {
         private Album albDisplayedAlbum;
+
+        private int cpt = 0;
+        private int album_length = 0;
+        
         public Diapo(Album alb)
         {
             InitializeComponent();
             this.albDisplayedAlbum = alb;
+            
             // détermination de la taille de l'album
-
             foreach (Picture t in albDisplayedAlbum.Pictures)
             {
-                j++;
+                album_length++;
             }
-        }
-
-
-        int i = 0;
-        int j = 0;
-              
-     
+        } 
+           // timer
            private void timer1_Tick(object sender, EventArgs e)
             {
-
-
-               i++;
-
-                //réinitialisation du compteur
-                if (j == i)
+               cpt++;
+                //réinitialisation du diapo
+                if (album_length == cpt)
                 {
-                    i = 0;
+                    cpt = 0;
                 }
-
                //affichage de l'image
                 if (pictureBox1.Image != null)
                 {
-                    pictureBox1.Image = albDisplayedAlbum.Pictures[i].Image;
+                    pictureBox1.Image = albDisplayedAlbum.Pictures[cpt].Image;
                 }
             }
             
-        
+        // affichage de la première image
         private void Diapo_Load(object sender, EventArgs e)
         {
-            //affichage de la 1ere image.
             pictureBox1.Image = albDisplayedAlbum.Pictures[0].Image;
         }
 
+        //gestion du bouton suivante
         private void toolStripButton3_Click(object sender, EventArgs e)
         {
             //blocage du timer, chargement de la photo suivante
-            timer1.Enabled = false;
+            timer1.Enabled = false;       
+            cpt++;
 
-            
-            i++;
-
-            //réinitialisation du compteur
-           if (j == i)
+            //réinitialisation du diapo
+           if (album_length == cpt)
             {
-                i = 0;
+                cpt = 0;
             }
 
-            //affichage de l'image
+           //affichage de l'image
            if (albDisplayedAlbum.Pictures.Count > 0)
            {
-               pictureBox1.Image = albDisplayedAlbum.Pictures[i].Image;
+               pictureBox1.Image = albDisplayedAlbum.Pictures[cpt].Image;
            }
-
             //on remet le timer en route
             timer1.Enabled = true;
         }
 
+        //gestion du bouton précédent
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
             //même système mais dans l'autre sens
-
-
             timer1.Enabled = false;
 
-            //réinitialisation du compteur
-            if (i == 0)
+            //réinitialisation du diapo
+            if (cpt == 0)
             {
-                i = j;
+                cpt = album_length;
             }
 
-            i--;
+            cpt--;
 
             //affichage de l'image
             if (albDisplayedAlbum.Pictures.Count > 0)
             {
-                pictureBox1.Image = albDisplayedAlbum.Pictures[i].Image;
+                pictureBox1.Image = albDisplayedAlbum.Pictures[cpt].Image;
             }
                 timer1.Enabled = true;
         }
-
+        // Gestion des flèches droite et gauche
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
             bool ret = true;
@@ -110,30 +101,30 @@ namespace viewer
                     {
                         case Keys.Right:
                             {
-                                i++;
+                                cpt++;
 
-                                //réinitialisation du compteur
-                                if (j == i)
+                                //réinitialisation du diapo
+                                if (album_length == cpt)
                                 {
-                                    i = 0;
+                                    cpt = 0;
                                 }
 
                                 //affichage de l'image
-                                pictureBox1.Image = albDisplayedAlbum.Pictures[i].Image;
+                                pictureBox1.Image = albDisplayedAlbum.Pictures[cpt].Image;
                             }
                             break;
                         case Keys.Left:
                            {
-                                //réinitialisation du compteur
-                                if (i == 0)
+                               //réinitialisation du diapo
+                                if (cpt == 0)
                                 {
-                                    i = j;
+                                    cpt = album_length;
                                 }
 
-                                i--;
+                                cpt--;
 
                                 //affichage de l'image
-                                pictureBox1.Image = albDisplayedAlbum.Pictures[i].Image;
+                                pictureBox1.Image = albDisplayedAlbum.Pictures[cpt].Image;
                             }
                             break;
                         case Keys.Up:  break;
