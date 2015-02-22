df2e575ef4dac322af90e7409ea686b4ceac6e76
thom78 <thomthom78@live.fr>
2015-02-22 16:52:58 +0100

    Ajout Diapo options Vitesses + Pause

--- viewer/GUI/Forms/Diapo.Designer.cs
+++ viewer/GUI/Forms/Diapo.Designer.cs
@@ -35,6 +35,11 @@
             this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.vitesseDiapoRapideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vitesseMoyenneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vitessseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -45,10 +50,12 @@
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripButton1,
-            this.toolStripButton3});
-            this.toolStrip1.Location = new System.Drawing.Point(561, 715);
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripSplitButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(517, 440);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(58, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(113, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -75,7 +82,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 2000;
+            this.timer1.Interval = 4000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // pictureBox1
@@ -85,17 +92,61 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1173, 749);
+            this.pictureBox1.Size = new System.Drawing.Size(1085, 474);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vitesseDiapoRapideToolStripMenuItem,
+            this.vitesseMoyenneToolStripMenuItem,
+            this.vitessseToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 22);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            // 
+            // vitesseDiapoRapideToolStripMenuItem
+            // 
+            this.vitesseDiapoRapideToolStripMenuItem.Name = "vitesseDiapoRapideToolStripMenuItem";
+            this.vitesseDiapoRapideToolStripMenuItem.Size = new System.Drawing.Size(247, 22);
+            this.vitesseDiapoRapideToolStripMenuItem.Text = "vitesse du diaporama - rapide";
+            this.vitesseDiapoRapideToolStripMenuItem.Click += new System.EventHandler(this.vitesseDiapoRapideToolStripMenuItem_Click);
+            // 
+            // vitesseMoyenneToolStripMenuItem
+            // 
+            this.vitesseMoyenneToolStripMenuItem.Name = "vitesseMoyenneToolStripMenuItem";
+            this.vitesseMoyenneToolStripMenuItem.Size = new System.Drawing.Size(247, 22);
+            this.vitesseMoyenneToolStripMenuItem.Text = "vitesse du diaporama - moyenne";
+            this.vitesseMoyenneToolStripMenuItem.Click += new System.EventHandler(this.vitesseMoyenneToolStripMenuItem_Click);
+            // 
+            // vitessseToolStripMenuItem
+            // 
+            this.vitessseToolStripMenuItem.Name = "vitessseToolStripMenuItem";
+            this.vitessseToolStripMenuItem.Size = new System.Drawing.Size(247, 22);
+            this.vitessseToolStripMenuItem.Text = "vitesse du diaporama - lente";
+            this.vitessseToolStripMenuItem.Click += new System.EventHandler(this.vitessseToolStripMenuItem_Click);
             // 
             // Diapo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1173, 749);
+            this.ClientSize = new System.Drawing.Size(1085, 474);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Diapo";
@@ -118,5 +169,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem vitesseDiapoRapideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vitesseMoyenneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vitessseToolStripMenuItem;
     }
 }
